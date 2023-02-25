e1f3e3b27c269e5dc9bd88a20423f82f12428cec
codecypherz <james.deyerle@gmail.com>
2023-02-25 16:38:11 -0500

    Lined up the pieces.

--- src/model/game.service.ts
+++ src/model/game.service.ts
@@ -36,21 +36,21 @@ export class GameService {
     const board = this.board;
     // Player 1 pieces
     let player1 = this.turnService.player1;
-    this.addPiece(0, 2, new Lancehorn(board, player1));
-    this.addPiece(0, 4, new Charger(board, player1));
+    this.addPiece(0, 1, new Lancehorn(board, player1));
+    this.addPiece(0, 2, new Charger(board, player1));
     this.addPiece(0, 3, new Bristleback(board, player1));
-    this.addPiece(1, 2, new Widemaw(board, player1));
-    this.addPiece(1, 4, new Burrower(board, player1));
-    this.addPiece(0, 0, new Glinthawk(board, player1));
+    this.addPiece(0, 4, new Widemaw(board, player1));
+    this.addPiece(0, 5, new Burrower(board, player1));
+    this.addPiece(0, 6, new Glinthawk(board, player1));
 
     // Player 2 pieces
     let player2 = this.turnService.player2;
-    this.addPiece(5, 4, new Lancehorn(board, player2));
-    this.addPiece(7, 3, new Charger(board, player2));
+    this.addPiece(7, 6, new Lancehorn(board, player2));
+    this.addPiece(7, 5, new Charger(board, player2));
     this.addPiece(7, 4, new Bristleback(board, player2));
-    this.addPiece(2, 0, new Widemaw(board, player2));
-    this.addPiece(6, 2, new Burrower(board, player2));
-    this.addPiece(7, 7, new Glinthawk(board, player2));
+    this.addPiece(7, 3, new Widemaw(board, player2));
+    this.addPiece(7, 2, new Burrower(board, player2));
+    this.addPiece(7, 1, new Glinthawk(board, player2));
   }
 
   private addPiece(row: number, col: number, piece: Piece): void {
