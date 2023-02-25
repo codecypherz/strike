2d97c6cfdee537971545248182a6147e29a1cd77
codecypherz <james.deyerle@gmail.com>
2023-02-25 15:31:23 -0500

    Organized files in the model directory.

---
+++ src/model/machine/scrounger.ts
@@ -0,0 +1,29 @@
+import { Board } from "../board";
+import { Player } from "../player";
+import { MeleePiece } from "../piece/meleepiece";
+import { Piece } from "../piece/piece";
+
+export class Scrounger extends MeleePiece {
+
+  static NAME = 'Scrounger';
+  // Scrapper + Scrounger are the same image?
+  static IMAGE_URL = Piece.IMAGE_PATH + 'scrapper.webp';
+  static POINTS = 1;
+  static MOVEMENT = 3;
+  static ATTACK = 2;
+  static ATTACK_RANGE = 1;
+  static MAX_HEALTH = 5;
+
+  constructor(board: Board, player: Player) {
+    super(
+      board,
+      Scrounger.NAME,
+      Scrounger.IMAGE_URL,
+      Scrounger.POINTS,
+      Scrounger.MOVEMENT,
+      Scrounger.ATTACK,
+      Scrounger.ATTACK_RANGE,
+      Scrounger.MAX_HEALTH,
+      player);
+  }
+}
