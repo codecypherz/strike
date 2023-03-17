8d096cb7ffc0b557712dd80c3ac049c76c0d0557
codecypherz <james.deyerle@gmail.com>
2023-03-17 07:50:20 -0400

    npm update and install of uuid

--- src/model/game.ts
+++ src/model/game.ts
@@ -1,8 +1,11 @@
 import { Board } from "./board";
 import { PieceCollection } from "./piececollection";
+import { v4 as uuidv4 } from 'uuid';
 
 export class Game {
   
+  private id = uuidv4();
+
   constructor(public board: Board, public pieceCollection: PieceCollection) {
     this.board = board;
     this.pieceCollection = pieceCollection;
