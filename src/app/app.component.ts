e2d7fd2640d2f2f0c97949ff94fbd11de6c49162
codecypherz <james.deyerle@gmail.com>
2023-03-21 21:20:47 -0400

    Clicking title navigates home.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  constructor(private router: Router) { }
+
+  navigateHome(): void {
+    this.router.navigate(['/game-selection']);
+  }
 }
